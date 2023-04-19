cf004d443ed89cd782d9a461b54437d0f5122966
Rajan Lamba <rk1759593@gmail.com>
2023-04-19 15:47:42 +0530

    add post

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,7 +1,30 @@
+const postSchema = require("../models/post");
+
 module.exports.home = function (req, res) {
   // console.log(req.cookies);
   // res.cookie("id", 34);
-  return res.render("home", {
-    title: "home side",
-  });
+  // postSchema
+  //   .find({})
+  //   .then((posts) => {
+  //     return res.render("home", {
+  //       posts: posts,
+  //       title: "home side",
+  //     });
+  //   })
+  //   .catch((e) => {
+  //     console.log(e);
+  //   });
+
+  postSchema
+    .find({})
+    .populate("user")
+    .then((posts) => {
+      return res.render("home", {
+        posts: posts,
+        title: "home side",
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const postController = require("../controller/postController");
 
-router.post("/create", postController.postData);
+router.post("/create", passport.checkAthentication, postController.postData);
 
 module.exports = router;
