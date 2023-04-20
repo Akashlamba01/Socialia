0ba0f83cf0fda84319b538402afd3dc4685306b9
Rajan Lamba <rk1759593@gmail.com>
2023-04-20 18:48:19 +0530

    partials comment and post

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,3 +1,4 @@
+const { compile } = require("ejs");
 const Comment = require("../models/comment");
 const Post = require("../models/post");
 
@@ -28,5 +29,36 @@ module.exports.create = function (req, res) {
     })
     .catch((e) => {
       console.log(e);
+    });
+};
+
+module.exports.delete = function (req, res) {
+  // console.log(req.params.id)
+  console.log("req.param.id: ", req.user.id);
+  Comment.findById(req.params.id)
+    .then((comment) => {
+      console.log("comment.user", comment);
+      if (comment.user == req.user.id) {
+        let postId = comment.post;
+
+        comment.deleteOne({ _id: req.params.id });
+
+        Post.findByIdAndUpdate(postId, {
+          $pull: { comments: req.params.id },
+        })
+          .then(() => {
+            return res.redirect("back");
+          })
+          .catch((e) => {
+            console.log("err form delete comment side", e);
+            return res.redirect("back");
+          });
+      } else {
+        return res.redirect("back");
+      }
+    })
+    .catch((e) => {
+      console.log("err form delete comment side", e);
+      return res.redirect("back");
     });
 };
--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,10 @@ const passport = require("passport");
 const commentController = require("../controller/commentController");
 
 router.post("/create", passport.checkAthentication, commentController.create);
+router.get(
+  "/destroy/:id",
+  passport.checkAthentication,
+  commentController.delete
+);
 
 module.exports = router;
