cfb9acfea4ac049f5384b0ec3e5c07a50d1a17d5
Rajan Lamba <rk1759593@gmail.com>
2023-04-20 17:07:40 +0530

    delete post

--- controller/postController.js
+++ controller/postController.js
@@ -1,16 +1,46 @@
-const postSchema = require("../models/post");
+const PostSchema = require("../models/post");
+const CommentSchema = require("../models/comment");
 
 module.exports.postData = function (req, res) {
-  postSchema
-    .create({
-      content: req.body.content,
-      user: req.user._id,
-    })
+  PostSchema.create({
+    content: req.body.content,
+    user: req.user._id,
+  })
     .then((post) => {
       console.log(post);
       res.redirect("/");
     })
     .catch((e) => {
       console.log("err from the creating post side", e);
+    });
+};
+
+module.exports.delete = function (req, res) {
+  console.log(req.params.id);
+  PostSchema.findById(req.params.id)
+    .then((post) => {
+      // console.log("userid: ", post.user == req.params.id);
+      // console.log("user: ", post.user);
+      // console.log("user params: ", req.params.id);
+
+      if (post.user == req.user.id) {
+        console.log("post id: ", req.params.id);
+
+        post.deleteOne({ _id: post._id });
+
+        CommentSchema.deleteMany({ post: req.params.id })
+          .then((result) => {
+            // console.log(result);
+            return res.redirect("back");
+          })
+          .catch((e) => {
+            return res.redirect("back");
+          });
+      } else {
+        return res.redirect("back");
+      }
+    })
+    .catch((e) => {
+      console.log("error form delete side", e);
     });
 };
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,6 @@ const passport = require("passport");
 const postController = require("../controller/postController");
 
 router.post("/create", passport.checkAthentication, postController.postData);
+router.get("/destroy/:id", passport.checkAthentication, postController.delete);
 
 module.exports = router;
